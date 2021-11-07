b4e6a232c7485169441318f792f648575e6d996f
MineisZarox <85471432+MineisZarox@users.noreply.github.com>
2021-11-07 22:32:06 +0530

    Update pinterest.py

--- userbot/plugins/pinterest.py
+++ userbot/plugins/pinterest.py
@@ -7,6 +7,7 @@ import asyncio
 from userbot import catub
 from . import mention
 from ..core.managers import edit_or_reply
+from ..helpers.utils import reply_id
 from urllib import request
 try:
     from pyquery import PyQuery as pq
@@ -24,6 +25,7 @@ def get_download_url(link):
     download_url = pq(str_request_content)('table.table-condensed')('tbody')('td')('a').attr('href')
     return download_url
 
+
 @catub.cat_cmd(
     pattern="pint(?:\s|$)([\s\S]*)",
     command=("pint", plugin_category),
@@ -35,19 +37,28 @@ def get_download_url(link):
         ],
     },
 )
-async def _(event):
+
+async def pint(event):
     "To download pinterest posts"
-    A = "".join(event.text.split(maxsplit=1)[1:])
+    A = event.pattern_match.group(1)
+    B = await event.get_reply_message()
     reply_to_id = await reply_id(event)
-    links = re.findall(r"\bhttps?://.*\.\S+", A)
-    await event.delete()
-    if not links:
-        Y = await event.respond("`Please give a valid link`", reply_to=reply_to_id)
-        await asyncio.sleep(3)
-        await Y.delete()
-    else:
-        Z = await event.respond("`Downloading...`", reply_to=reply_to_id)
+    if A:
         MINE = get_download_url(A)
-        await event.client.send_file(event.chat.id, MINE, caption=f"➥Uploaded by = {mention}\n➥Pin = [Link]({A})", reply_to=reply_to_id)
-        await Z.delete()
+    elif B and B.message:
+    	MINE = get_download_url(B.message)
+    else:
+        return await edit_delete(event, "`Provide a link along with cmd or reply to a link `")
+    if A and "pint" not in A:
+        await edit_or_reply(event, "`Please give a valid pinterest link`")
+        return
+    elif not A and "pint" not in B.message:
+        await edit_or_reply(event, "`Please give a valid pinterest link`")
+        return
+    else:
+    	pass
+    await edit_or_reply(event, "`Downloading...`")
+    await event.delete()
+    await event.client.send_file(event.chat.id, MINE, caption=f"➥Uploaded by = {mention}", reply_to=reply_to_id)
+    
     
