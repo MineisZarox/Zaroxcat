5a5b0c124360fc18ce5a66176f46e202eea07740
MineisZarox <85471432+MineisZarox@users.noreply.github.com>
2021-12-05 21:27:55 +0530

    Update custom.py

--- userbot/plugins/custom.py
+++ userbot/plugins/custom.py
@@ -22,7 +22,7 @@ vlist = [
     "ALLOW_NSFW",
     "EXTRA_PIC",
     "HELP_EMOJI",
-    "HELP_PIC"
+    "HELP_PIC",
     "HELP_TEXT",
     "IALIVE_PIC",
     "INLINE_PIC",
@@ -33,7 +33,7 @@ vlist = [
     "START_TEXT",
     "NO_OF_ROWS_IN_HELP",
     "NO_OF_COLUMNS_IN_HELP",
-    "CUSTOM_STICKER_PACKNAME"",
+    "CUSTOM_STICKER_PACKNAME",
 ]
 
 oldvars = {
