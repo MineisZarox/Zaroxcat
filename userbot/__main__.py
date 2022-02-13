d8a2dc99e61ab690c116765548d6e413c0ea4110
MineisZarox <85471432+MineisZarox@users.noreply.github.com>
2022-02-13 22:19:59 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -35,7 +35,7 @@ except Exception as e:
 repo = os.environ.get("EXTERNAL_PLUGIN_REPO") or "https://github.com/MineisZarox/Plugins"
 token = os.environ.get("GITHUB_ACCESS_TOKEN")
 a, b, c, username, d, = plug_repo.split("/")
-ppr = str(plug_repo)[-8:]
+ppr = str(repo)[-8:]
 if token:
     plug_repo = f"https://{username}:{token}@{ppr}.git"
 else:
