7506824cccae0818286f0826b5436fb59a0f4dec
MineisZarox <85471432+MineisZarox@users.noreply.github.com>
2022-02-19 21:43:41 +0530

    Update refresh.py

--- userbot/plugins/refresh.py
+++ userbot/plugins/refresh.py
@@ -8,7 +8,6 @@ from . import catub, edit_delete, edit_or_reply, UPSTREAM_REPO_URL
 
 plugin_category = "tools"
 
-z, y, x, w, v, = UPSTREAM_REPO_URL.split("/")
 branch = Config.UPSTREAM_REPO_BRANCH or "master"
 # =================================================
 Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
@@ -30,7 +29,7 @@ else:
 # =================================================
     
 @catub.cat_cmd(
-    pattern="(refresh|re)( -def)$",
+    pattern="refresh( -def|$)?",
     command=("refresh", plugin_category),
     info={
         "header": "To refresh all external plugin.",
@@ -40,35 +39,48 @@ else:
 )
 
 async def refesh(event):
-    if_def = event.pattern_match.group(2)
-    if if_def:
-        exact_fld = "plugins"
-        repo = f"--single-branch --branch {branch} {UPSTREAM_REPO_URL}"
-        fld = f"{v}/userbot/plugins"
-        repo_fld = v
-    else:
-        exact_fld = "ext_plugins"
-        repo = plug_repo
-        fld = f"{d}/ext/plugins"
-        repo_fld = d
-    try:
-        k = os.listdir(f"userbot/{exact_fld}")
-        res = [sub.replace('.py', '') for sub in k]
-        for i in res:
-            rem(i)
-        sl.rmtree(f"userbot/{exact_fld}")
-        os.system(f"git clone {repo}")
-        os.system(f"mv '{fld}' 'userbot'")
-        sl.rmtree(repo_fld)
-        print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-        await load_plugins("ext_plugins")
-        print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    if_def = event.pattern_match.group(1)
+    if if_def == " -def":
         try:
-            Heroku = heroku3.from_key(HEROKU_API_KEY)
-            app = Heroku.app(HEROKU_APP_NAME)
-            data = app.get_log()
-            await edit_or_reply(event, data, deflink=True, linktext="`Refreshed all external plugins successfully: `")
-        except BaseException:
-            return await edit_or_reply(event, "`Refreshed all external plugins successfully`")
-    except Exception as e:
-        LOGS.error(f"{e}")
+            k = os.listdir("userbot/plugins")
+            res = [sub.replace('.py', '') for sub in k]
+            for i in res:
+                rem(i)
+            sl.rmtree("userbot/plugins")
+            os.system(f"git clone --single-branch --branch {branch} {UPSTREAM_REPO_URL}")
+            os.system("mv 'Zaroxcat/userbot/plugins' 'userbot'")
+            sl.rmtree("Zaroxcat")
+            print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+            await load_plugins("plugins")
+            print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+            try:
+                Heroku = heroku3.from_key(HEROKU_API_KEY)
+                app = Heroku.app(HEROKU_APP_NAME)
+                data = app.get_log()
+                await edit_or_reply(event, data, deflink=True, linktext="`Refreshed all default plugins successfully: `")
+            except BaseException:
+                return await edit_or_reply(event, "`Refreshed all default plugins successfully`")
+        except Exception as e:
+            LOGS.error(f"{e}")
+    else:
+        try:
+            k = os.listdir("userbot/ext_plugins")
+            res = [sub.replace('.py', '') for sub in k]
+            for i in res:
+                rem(i)
+            sl.rmtree("userbot/ext_plugins")
+            os.system(f"git clone {plug_repo}")
+            os.system("mv 'Plugins/ext_plugins' 'userbot'")
+            sl.rmtree("Plugins")
+            print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+            await load_plugins("ext_plugins")
+            print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+            try:
+                Heroku = heroku3.from_key(HEROKU_API_KEY)
+                app = Heroku.app(HEROKU_APP_NAME)
+                data = app.get_log()
+                await edit_or_reply(event, data, deflink=True, linktext="`Refreshed all external plugins successfully: `")
+            except BaseException:
+                return await edit_or_reply(event, "`Refreshed all external plugins successfully`")
+        except Exception as e:
+            LOGS.error(f"{e}")
