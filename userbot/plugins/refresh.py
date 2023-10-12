e48400333bb0006bb08e9d914fd907cd84a04fee
Zarox <85471432+MineisZarox@users.noreply.github.com>
2023-10-12 19:40:32 +0530

    Update refresh.py

--- userbot/plugins/refresh.py
+++ userbot/plugins/refresh.py
@@ -1,21 +1,12 @@
-#By @IrisZarox
+#By Zarox https://t.me/zarox
 import os
-import heroku3
 import shutil as sl
 from ..Config import Config
 from ..utils import load_module, load_plugins, remove_plugin as rem
-from . import catub, edit_delete, edit_or_reply, UPSTREAM_REPO_URL
+from . import catub, edit_or_reply
 
 plugin_category = "tools"
 
-z, y, x, w, v, = UPSTREAM_REPO_URL.split("/")
-branch = Config.UPSTREAM_REPO_BRANCH or "master"
-# =================================================
-Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
-heroku_api = "https://api.heroku.com"
-HEROKU_APP_NAME = Config.HEROKU_APP_NAME
-HEROKU_API_KEY = Config.HEROKU_API_KEY
-# =================================================
 
 
 # =================================================
@@ -35,18 +26,21 @@ else:
     info={
         "header": "To refresh external plugin with repository.",
         "description": "Reinstall external plugins from external plugin repository.",
-        "usage": "{tr}refresh",
+        "usage": [
+            "{tr}refresh",
+            "{tr}refresh plugin"
+        ],
     },
 )
-
 async def refesh(event):
     "To refresh ext_plugins"
-    plugin = event.pattern_match.group(1)
-    if plugin:
+    if not os.environ.get("EXTERNAL_PLUGIN_REPO"):
+        return await edit_or_reply(event,  "Please set Enviroment variable `EXTERNAL_PLUGIN_REPO`\n\n__Note: This ENV VAR is only used by this perticular plugin.__")
+    if plugin := event.pattern_match.group(1):
         try:
             try:
                 rem(plugin)
-            except:
+            except Exception:
                 return await edit_or_reply(event, f"`No such plugin exist as {plugin}.py`")
             os.remove(f"userbot/ext_plugins/{plugin}.py")
             os.system(f"git clone {plug_repo}")
@@ -57,7 +51,6 @@ async def refesh(event):
             print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
             await edit_or_reply(event,  f"`Refreshed {plugin} successfully.`")
         except Exception as e:
-            LOGS.error(f"{e}")
             return await edit_or_reply(event,  f"`Error:: {e}`")
     else:
         try:
@@ -70,21 +63,12 @@ async def refesh(event):
             os.system(f"git clone {plug_repo}")
             os.system("mv 'Plugins/ext_plugins' 'userbot'")
             sl.rmtree("Plugins")
-            await edit_or_reply(event, f"`Installing external plugins...`")
+            await edit_or_reply(event, "`Installing external plugins...`")
             print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
             await load_plugins("ext_plugins")
             print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-            plug = 0
-            dir = os.listdir("userbot/ext_plugins")
-            for file in dir:
-                plug+=1
-            try:
-                Heroku = heroku3.from_key(HEROKU_API_KEY)
-                app = Heroku.app(HEROKU_APP_NAME)
-                data = app.get_log()
-                await edit_or_reply(event, data, deflink=True, linktext=f"`Refreshed all {plug} external plugins successfully:`")
-            except BaseException:
-                return await edit_or_reply(event, "`Refreshed all external plugins successfully`")
+            dir_ = os.listdir("userbot/ext_plugins")
+            plug = sum(1 for _ in dir_)
+            return await edit_or_reply(event, f"`Refreshed all {plug} external plugins successfully`")
         except Exception as e:
-            LOGS.error(f"{e}")
             return await edit_or_reply(event,  f"`Error: {e}`")
